21052c71ad8ed1e7ed9944448cc3b32e3acaa3f3
sukanya068 <ausukanya@hotmail.com>
2020-03-23 17:36:18 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Message</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from '../src/components/Header';
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Header title="Simple Firebase App" />
+      </div>
+    );
+  }
 }
-
 export default App;
